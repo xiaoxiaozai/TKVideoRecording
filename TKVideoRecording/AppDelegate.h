7c34463dc50ed72c3525993b4b15330662cb7bf0
xiaoxiaozai <foreverlosda@vip.qq.com>
2016-03-10 08:50:32 +0800

    create

---
+++ TKVideoRecording/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TKVideoRecording
+//
+//  Created by 翟宇 on 16/3/10.
+//  Copyright © 2016年 翟宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TKVideoRecording/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TKVideoRecording
+//
+//  Created by 翟宇 on 16/3/10.
+//  Copyright © 2016年 翟宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
